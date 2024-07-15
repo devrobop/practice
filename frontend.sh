b3e046198af16b3ea8b923775df081a7492ef024
prudhvi-1122 <prudhwi1998@gmail.com>
2024-07-15 15:25:37 +0530

    changes in frontend

--- frontend.sh
+++ frontend.sh
@@ -3,8 +3,6 @@ component=frontend
 app_path=/usr/share/nginx/html/    
 
 
-
-
 PRINT Nginx Disable Default version
 dnf module disable nginx -y &>>$LOG_FILE
 STAT $?
@@ -13,24 +11,17 @@ PRINT Enable nginx module 1.24
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
 STAT $?
 
-
 PRINT Install Nginx
 dnf install nginx -y &>>$LOG_FILE
 STAT $?
-
 
 PRINT COPY Nginx config file 
 cp nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
 STAT $?
 
-
-PRINT Enabling Nginx
-systemctl enable nginx &>>$LOG_FILE
-STAT $?
-
-
 APP_PREREQ
 
-PRINT Restart Nginx service
+PRINT start Nginx service
+systemctl enable nginx &>>$LOG_FILE
 systemctl restart nginx &>>$LOG_FILE
 STAT $?
